8f3ea68be3c31fb91266febdf8bff40fd5d69cfe
quancoi2ka3 <106460115+quancoi2ka3@users.noreply.github.com>
2023-03-06 21:52:18 +0700

    Add files via upload
    thiepmom

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "Mẹ Hiền";
+const giftUrl = "https://www.facebook.com/myle.nguyen.71653318";
+const eventName = "Chúc Mừng 8-3";
+const titleCard = "Send To Mom";
+const contentCard =
+  "Con chúc mẹ ngày 8.3 thật vui vẻ, hạnh phúc và thật nhiều sức khỏe, mẹ phải cười nhiều lên nhé cho dù mẹ phải lo toan những bộn bề của cuộc sống, chăm lo cho gia đình mình ";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "mom.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
